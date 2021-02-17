cee85e6656af42b67a443b40002808f6b94d66b5
kevin <kevin.melan00@usc.edu.co>
2021-02-17 03:38:54 -0500

    Set timeout in auth socket controller

--- src/shared/sockets/controllers/auth-socket/auth-socket.controller.js
+++ src/shared/sockets/controllers/auth-socket/auth-socket.controller.js
@@ -4,21 +4,23 @@ const { getUser } = require('../../../../components/auth/services/auth.service')
 const authSocketController = (socket, next) => {
   const query = socket.handshake.query;
   const { token } = query;
-  verifyToken(token)
-      .then(({ payload }) => {
-        return getUser(payload._id);
-      })
-      .then(user => {
-        if (!user) {
-          return socket.disconnect();
-        }
-        socket.request.user = user;
-        socket.id = user._id;
-        next();
-      })
-      .catch(err => {
+  setTimeout(() => {
+    verifyToken(token)
+    .then(({ payload }) => {
+      return getUser(payload._id);
+    })
+    .then(user => {
+      if (!user) {
         return socket.disconnect();
-      });
+      }
+      socket.request.user = user;
+      socket.id = user._id;
+      next();
+    })
+    .catch(err => {
+      return socket.disconnect();
+    });
+  }, 50);
 }
 
 module.exports = {
